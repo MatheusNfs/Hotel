b4638d12dcec2108a9ea2b9a5b35d3129c202298
Matheus Nascimento Ferreira da Silva <matheus.dev2022@gmail.com>
2023-02-17 16:45:15 -0300

    Projeto Hotel

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/reset.css">
+    <link rel="stylesheet" href="style/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Gudea:ital,wght@0,400;0,700;1,400&family=Gugi&family=Montserrat:wght@100&display=swap" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body style="background-image: url('img/aerialview.jpg');">
+    <header>
+        <menu>
+            <div class="logo">
+                <h1 class="overlogo">UIRAPURU</h1>
+                <p class="underlogo">beach resort</p>
+            </div>
+            <ul class="menulist">
+                <li class="menuitem"><a href="" class="menulink">THE RESORT</a></li>
+                <li class="menuitem"><a href="" class="menulink">ACCOMMODATIONS</a></li>
+                <li class="menuitem"><a href="" class="menulink">ACTIVITIES</a></li>
+                <li class="menuitem"><a href="" class="menulink">GASTRONOMY</a></li>                
+                <li class="menuitem"><a href="" class="menulink">RESERVE NOW</a></li>                
+            </ul>
+        </menu>
+    </header>
+    <main>
+        <p class="frontpgtxt1">Experience Tranquility...</p>
+        <img src="img/hotelpool.jpeg" alt="The Hotel main Pool"><img src="img/beachview.jpeg" alt="the view from the front of the hotel">
+        <p class="frontpgtxt2">...The Vacation of a Lifetime</p>
+    </main>
+    <footer>
+        <h3>Uirapuru Beach Resort</h3>
+    </footer>  
+</body>
+</html>
